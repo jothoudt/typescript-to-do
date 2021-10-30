25f922f531e3b7d7019d9de248c52900e4c2ba84
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-10-29 22:44:13 -0500

    update some of the styling

--- src/Components/AddTask/AddTask.tsx
+++ src/Components/AddTask/AddTask.tsx
@@ -1,5 +1,8 @@
 import React,{useState} from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import './AddTask.css'
 //this component is for adding tasks to the list
 function AddTask(){
     //define dispatch
@@ -20,10 +23,17 @@ function AddTask(){
         dispatch({type:'ADD_NEW_TASK', payload:taskToAdd})
     }   //end addThisTaks
     return(
-        <div>
-            <input type="text" placeholder="Task" onChange={(event)=>{setTask(event.target.value)}} />
-            <button onClick={addThisTask}>Add Task</button>
-        </div>
+        <Box border={1} borderColor="#deb887" boxShadow={12} style={{width:"400px", height:"200px", marginTop:"50px", marginLeft:"auto", marginRight:"auto", padding:"25px"}}>
+            <div>
+                <h2>Add A Task</h2>
+            </div>
+            <div>
+                <input className={task === '' ? "add-task-input" : "text-entered-input"} type="text" placeholder="Task" onChange={(event)=>{setTask(event.target.value)}} />
+            </div>
+            <div>
+                <Button style={{backgroundColor:"#deb887", marginTop: "24px", fontSize: "large"}}onClick={addThisTask}>Add Task</Button>
+            </div>
+        </Box>
     )
 };  //end AddTask
 
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -15,9 +15,6 @@ function Home(){
     return(
     <div>
         <div>
-            <h1>Hello World</h1>
-        </div>
-        <div>
             <AddTask />
         </div>
         <div>
